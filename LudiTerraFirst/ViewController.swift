6c541907527e33127907563269a2cc3ce5fa7145
Yael_Amaya <yiap2610@gmail.com>
2017-10-06 12:26:31 -0600

    Ludi Terra Start

---
+++ LudiTerraFirst/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LudiTerraFirst
+//
+//  Created by Amaya Penunuri, Yael on 10/6/17.
+//  Copyright © 2017 Amaya Penunuri, Yael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
