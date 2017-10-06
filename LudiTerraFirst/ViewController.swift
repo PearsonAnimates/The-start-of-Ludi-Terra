49be910f6a635939490aa6c9dd962c574872b9ae
Yael_Amaya <yiap2610@gmail.com>
2017-10-06 13:10:01 -0600

    made button turn orange if black, and black if orange! Happy halloween.
    what am I doing with ma life? XD

--- LudiTerraFirst/ViewController.swift
+++ LudiTerraFirst/ViewController.swift
@@ -12,11 +12,25 @@ class ViewController: UIViewController
 {
     @IBOutlet weak var firstButton: UIButton!
     
+    @IBAction func firstMethod(_ sender: UIButton)
+    {
+        firstButton.backgroundColor = .green
+        if ( firstButton.backgroundColor == .orange)
+        {
+            firstButton.backgroundColor = .black
+        }
+        else
+        {
+            firstButton.backgroundColor = .orange
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
